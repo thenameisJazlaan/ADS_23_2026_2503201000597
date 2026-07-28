a28732b49be1af79be121c2b8f405edbe4bacbba
Jazlaan Gazi <jazlaangazijaxx@gmail.com>
2026-07-28 13:04:25 +0530

    postfix

---
+++ stringpostfix.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int precedence(char op)
+{
+    if (op == '^')
+        return 3;
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    string infix = "(a+(b*(c^d)))";
+    string postfix = "";
+
+    stack<char> st;
+
+    for (int i = 0; i < infix.length(); i++)
+    {
+        char ch = infix[i];
+
+        
+        if (isalnum(ch))
+        {
+            postfix += ch;
+        }
+
+        
+        else if (ch == '(')
+        {
+            st.push(ch);
+        }
+
+        
+        else if (ch == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                postfix += st.top();
+                st.pop();
+            }
+            st.pop();   
+        }
+
+        
+        else
+        {
+            while (!st.empty() && precedence(st.top()) >= precedence(ch))
+            {
+                postfix += st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
+    }
+
+    
+    while (!st.empty())
+    {
+        postfix += st.top();
+        st.pop();
+    }
+
+    cout << "Postfix = " << postfix << endl;
+
+    return 0;
+}
